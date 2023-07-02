cb5ed7646d152d3879eb6050cfce0aeea72c17fd
LimNakHyun <dlaskrgus8@gmail.com>
2023-07-03 01:02:05 +0900

    르탄이 빗방울 받기 게임 끝내기 (7/8)

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject rain;
+    public GameObject panel;
     public static GameManager I;
     public Text scoreText;
+    public Text timeText;
     int totalScore;
+    float limit;
 
     private void Awake()
     {
@@ -18,7 +22,16 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("makeRain", 0, 0.1f);
+        initGame();
+        InvokeRepeating("makeRain", 0, 0.5f);
+    }
+
+
+    void initGame()
+    {
+        Time.timeScale = 1.0f;
+        totalScore = 0;
+        limit = 5.0f;
     }
 
     void makeRain()
@@ -29,12 +42,24 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        limit -= Time.deltaTime;
+        if(limit < 0)
+        {
+            Time.timeScale = 0.0f;
+            panel.SetActive(true);
+            limit = 0.0f;
+        }
+        timeText.text = limit.ToString("N2");
     }
 
     public void addScore(int score)
     {
         totalScore += score;
         scoreText.text = totalScore.ToString();
+    }
+
+    public void retry()
+    {
+        SceneManager.LoadScene("MainScene");
     }
 }
